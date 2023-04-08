063e4e97ca8e81159661aba89a88b360a52cd72a
VicE00 <vespeche@fi.uba.ar>
2023-04-08 14:03:20 -0300

    Agrego los metodos para frecuencia y repeticios. Cambio los test un poco

--- src/main/java/Evento.java
+++ src/main/java/Evento.java
@@ -5,27 +5,30 @@ public class Evento
 {
     private String titulo;
     private String descripcion;
-    private LocalDateTime    fechaInicio;
-    private LocalDateTime    fechaFin;
+    private LocalDateTime fechaInicio;
+    private LocalDateTime fechaFin;
+    private String repetir;
+    private int   frecuencia;
 
-    public Evento()
+
+    public Evento() //Constructor default
     {
         this.titulo = "Evento sin titulo";
         this.descripcion = "-";
         this.fechaInicio = LocalDateTime.now();
         this.fechaFin = this.fechaInicio.plusDays(1);
-
-        //Si no tengo titulo/descripcion, las pruebas no se pasan
-        //this.fechaInicio = LocalDateTime.of(-999999999,1,1,0,0 ); //Chequear
-        //this.fechaFin = LocalDateTime.of(999999999,12,31,23,59 ); //El evento puede terminar el mismo dia
+        this.repetir = "Diario";
+        this.frecuencia = 1;
     }
 
-    public Evento(String titulo, String descripcion, LocalDateTime fechaInicio, LocalDateTime fechaFin)
+    public Evento(String titulo, String descripcion, LocalDateTime fechaInicio, LocalDateTime fechaFin, String repetir, int frecuencia)
     {
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
+        this.repetir = repetir;
+        this.frecuencia = frecuencia;
 
     }
 //GETTERS
@@ -34,6 +37,9 @@ public class Evento
     public String obtenerDescripcion() {return descripcion;}
     public LocalDateTime obtenerFechaInicio() {return fechaInicio;}
     public LocalDateTime obtenerFechaFin() {return fechaFin;}
+    public String obtenerRepetir() {return repetir;}
+    public int obtenerFrecuencia(){return frecuencia;}
+
 
 
 
@@ -51,4 +57,8 @@ public class Evento
     public void establecerFechaFin(LocalDateTime fechaFin) {
         this.fechaFin = fechaFin;
     }
+    public void establecerRepetir(String repetir) {this.repetir = repetir;}
+    public void establecerFrecuencia(int frecuencia) {this.frecuencia = frecuencia;}
 }
+
+
--- src/test/java/EventoTest.java
+++ src/test/java/EventoTest.java
@@ -26,18 +26,49 @@ public class EventoTest
         assertEquals("-", descripcion);
     }
 
+
+
+    //testeo el constructor default
+   @Test
+    public void testEventoEnBlanco() //En este caso, el evento esta en blanco y es de dia completo
+    {
+        // Arrange
+        var evento = new Evento();
+
+        // Act
+        String titulo = evento.obtenerTitulo();
+        String descripcion = evento.obtenerDescripcion();
+        String repetir = evento.obtenerRepetir();
+        int frecuencia = evento.obtenerFrecuencia();
+
+        // Assert
+        assertEquals("Evento sin titulo",titulo);
+        assertEquals("-", descripcion);
+        assertEquals("Diario", repetir);
+        assertEquals(1, frecuencia);
+        assertEquals(LocalDateTime.now().toLocalDate(), evento.obtenerFechaInicio().toLocalDate());
+        assertEquals(LocalDateTime.now().plusDays(1).toLocalDate(), evento.obtenerFechaFin().toLocalDate());
+    }
+
+    //Testeo el constructor principal
     @Test
     public void creacionEventoNormal() //Este evento empieza un dia y termina otro
     {
-        var fechaInicio = LocalDateTime.of(2023, 4, 1, 7, 0);
+        String titulo = "Examenes";
+        String descripcion = "Semana de examenes";
+        var fechaInicio = LocalDateTime.of(2023, 4, 1, 12, 30);
         var fechaFin = LocalDateTime.of(2023, 4, 8, 12, 30);
+        String repetir = "Semanal";
+        int frecuencia = 2;
 
-        var evento = new Evento("Examenes","Semana de examenes" ,fechaInicio, fechaFin);
+        var evento = new Evento(titulo, descripcion,fechaInicio, fechaFin, repetir, frecuencia);
 
         assertEquals("Examenes", evento.obtenerTitulo());
         assertEquals("Semana de examenes", evento.obtenerDescripcion());
         assertEquals(fechaInicio, evento.obtenerFechaInicio());
         assertEquals(fechaFin, evento.obtenerFechaFin());
+        assertEquals(repetir, evento.obtenerRepetir());
+        assertEquals(frecuencia, evento.obtenerFrecuencia());
     }
 
 }
