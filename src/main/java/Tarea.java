f4230a9fbad5a5d89842857e18bd10414a148c82
Matias <matmorales@fi.uba.ar>
2023-04-11 01:37:45 -0300

    Arranco con los metodos de La clase Tarea

--- src/main/java/Tarea.java
+++ src/main/java/Tarea.java
@@ -1,2 +1,61 @@
-public class Tarea {
+import java.time.LocalDateTime;
+
+public class Tarea
+{
+    private String titulo;
+    private String descripcion;
+    private LocalDateTime fechaInicio;
+    private int repetirCada;
+
+
+    public Tarea()
+    {
+        this.titulo = "Tarea sin titulo";
+        this.descripcion = "-";
+        this.fechaInicio = LocalDateTime.now();
+        this.repetirCada = 0;
+
+    }
+
+    public Tarea(String titulo, String descripcion, LocalDateTime fechaInicio, LocalDateTime fechaFin)
+    {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fechaInicio = fechaInicio;
+        //RepetirCada deberia ir aca, estoy probando aun y lo voy a agregar al final. Por ahora solo lo seteo en main/tests :)
+    }
+
+    //GETTERS
+    public String obtenerTitulo() {
+        return titulo;
+    }
+
+    public String obtenerDescripcion()
+    {return descripcion;
+    }
+    public LocalDateTime obtenerFechaInicio() {
+        return fechaInicio;
+    }
+    public int obtenerRepetirCada(){
+        return repetirCada;}
+
+
+
+
+
+
+    public void establecerTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public void establecerDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public void establecerFechaInicio(LocalDateTime fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public void establecerRepetirCada(int repetirCada){
+        this.repetirCada = repetirCada;
+    }
 }
+
