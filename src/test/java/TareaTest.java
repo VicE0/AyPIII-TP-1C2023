116a696579d68465f6773c22b8fba57102693e8c
Matias <matmorales@fi.uba.ar>
2023-04-16 22:19:20 -0300

    Empezamos con las pruebas de tareas

--- src/test/java/TareaTest.java
+++ src/test/java/TareaTest.java
@@ -1,5 +1,32 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TareaTest {
+    @Test
+    public void tareaSinTitulo(){
+        var tarea = new Tarea();
+        String titulo = tarea.obtenerTitulo();
+        assertEquals("Tarea sin nombre", titulo);
+
+    }
+
+    @Test
+    public void tareaSinDescpcion(){
+        var tarea = new Tarea();
+        String descripcion = tarea.obtenerDescripcion();
+        assertEquals("",descripcion);
+    }
+
+    @Test
+    public void tareaNoCompletada(){
+        var tarea = new Tarea();
+        boolean completa = tarea.estaCompleta();
+        assertFalse(completa);
+    }
+
+
+
+
 
 }
